3d17da7898a2ef2a58e05c9d3cc3a9aa911ed808
Jamal Frank <jamaljfrank@gmail.com>
2020-02-16 23:43:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,10 +19,12 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
   
-  get '/multiply/:num1/:num2' do
-    @number1 = params[:num1].to_i
-    @number2 = params[:num2].to_i
-    (@number1 * @number2)
+  get "/multiply/:num1/:num2" do
+    @num1 = params[:num1]
+    @num2 = params[:num2]
+    product = (@num1.to_i * @num2.to_i)
+    product.to_s
   end
+
   
 end
